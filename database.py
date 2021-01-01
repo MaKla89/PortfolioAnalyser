b88c956306612029a58c7f1a27e655b22964e516
klaes <klaes.marcel@gmail.com>
2021-01-01 20:12:15 +0100

    Initial Release

---
+++ database.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import requests
+
+
+class Portfolio:
+    def __init__(self):
+        self.crypto_api_url = "https://api.coingecko.com/api/v3/simple/price?ids="
+        # self.yahoo_api_url = "https://yahoo-finance-low-latency.p.rapidapi.com/v6/finance/quote"  # Alternative API
+        self.yahoo_api_url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes"
+        self.portfolio = pd.DataFrame()
+        self.load_portfolio()
+        self.total_worth = 0
+        self.total_investment = 0
+        self.total_profit_loss = 0
+        self.total_profit_loss_rel = 0
+        self.f = open("api_key.txt", "r")           # Reads your Rapid-API-Key from .txt-File in same directory
+        self.api_key = self.f.read()
+
+    def load_portfolio(self):
+        try:
+            self.portfolio = pd.read_csv("portfolio.csv", sep=";", decimal=",")     # "portfolio.csv" must be provided!
+            print("Loading Portfolio-Information from .csv was successful!")
+
+        except FileNotFoundError:
+            print("Could not load any stored Portfolio. Will use DEMO-Portfolio!")
+            self.portfolio = pd.read_csv("demo-portfolio.csv", sep=";", decimal=",")
+
+    def update_portfolio(self):
+        stocks = []
+        for index, position in self.portfolio.iterrows():
+            if position["Stock or Crypto"] == "stock":
+                stocks.append(str(position["Symbol"]))
+        stocks = ", ".join(stocks)
+
+        querystring = {"region": "DE", "symbols": str(stocks)}
+        headers = {
+            'x-rapidapi-key': self.api_key,
+            'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
+        }
+        try:
+            stocks_response = requests.request("GET", self.yahoo_api_url, headers=headers, params=querystring)
+            stocks_response = stocks_response.json()
+
+        except Exception:
+            print(" ---------- Exception! ---------- ")
+            print("Probably a problem with Yahoo-API. See API-response:")
+            print(stocks_response)
+            print(" -------------------------------- ")
+
+        for index, position in self.portfolio.iterrows():
+
+            if position["Stock or Crypto"] == "crypto":
+
+                try:
+                    crypto_response = requests.get((str(self.crypto_api_url) +
+                                              str(position["Symbol"]) +
+                                              "&vs_currencies=eur"))
+                    crypto_response = crypto_response.json()
+                    new_price = crypto_response[str(position["Symbol"])]["eur"]
+
+                except Exception:
+                    print(" ---------- Exception! ---------- ")
+                    print("Probably a problem with CoinGecko-API. See API-response:")
+                    print(crypto_response)
+                    print(" -------------------------------- ")
+                    new_price = 0
+
+                new_price = float(new_price)
+                self.portfolio.at[index,"Current Price"] = new_price
+                current_value = float(float(position["Amount"])*new_price)
+                current_value = round(current_value, 2)
+                self.portfolio.at[index, "Current Value"] = current_value
+                win_loss = current_value - float(position["Investment"])
+                win_loss = round(win_loss, 2)
+                self.portfolio.at[index, "Profit / Loss"] = win_loss
+
+            elif position["Stock or Crypto"] == "stock":
+                response_segment = next(x for x in stocks_response["quoteResponse"]["result"] if x["symbol"] == position["Symbol"])
+                new_price = float(response_segment["regularMarketPrice"])
+                self.portfolio.at[index,"Current Price"] = new_price
+                current_value = float(float(position["Amount"])*new_price)
+                current_value = round(current_value, 2)
+                self.portfolio.at[index, "Current Value"] = current_value
+                win_loss = current_value - float(position["Investment"])
+                win_loss = round(win_loss, 2)
+                self.portfolio.at[index, "Profit / Loss"] = win_loss
+
+        self.total_worth = round(self.portfolio.sum()["Current Value"], 2)
+        self.total_investment = round(self.portfolio.sum()["Investment"], 2)
+        self.total_profit_loss = round(self.portfolio.sum()["Profit / Loss"], 2)
+        self.total_profit_loss_rel = (self.total_worth - self.total_investment)/self.total_investment*100
+        self.total_profit_loss_rel = round(self.total_profit_loss_rel, 1)
+
+
+if __name__ == '__main__':
+    port = Portfolio()
+    port.update_portfolio()
+    print(port.portfolio.to_string())
---
+++ main.py
@@ -0,0 +1,127 @@
+import database
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+port = database.Portfolio()
+port.update_portfolio()
+df = port.portfolio
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = dash.Dash(external_stylesheets=external_stylesheets, title="Portfolio Analyser")
+
+allocation = px.pie(df, values = "Current Value", names="Name")
+holdings_perf = px.bar(df, x="Name", y="Profit / Loss")
+
+app.layout = html.Div(children=[
+    html.H1(children="Portfolio Analyser",
+            style={"textAlign": "center"}),
+
+    html.Div([
+        html.Button(children="Load / Refresh Data", id='refresh', n_clicks=0, style={"margin-top": 20}),
+    ], style={"text-align": "center", "margin-bottom": 50}),
+
+    html.Div(className="row", style={"textAlign": "center", "font-size": "2em"}, children=[
+        html.Div(className="three columns", children=[
+            html.H2(children="Total Value"),
+        ]),
+
+        html.Div(className="three columns", children=[
+            html.H2(children="Total Investment"),
+        ]),
+
+        html.Div(className="three columns", children=[
+            html.H2(children="Absolute Profit"),
+        ]),
+
+        html.Div(className="three columns", children=[
+            html.H2(children="Relative Profit"),
+        ]),
+    ]),
+
+    html.Div(className="row", style={"textAlign": "center", "font-size": "2em"}, children=[
+        html.Div(className="three columns", children=[
+            html.P(id="total_value", children=str(port.total_worth) + " €")
+        ]),
+
+        html.Div(className="three columns", children=[
+            html.P(id="total_invest", children=str(port.total_investment) + " €")
+        ]),
+
+        html.Div(className="three columns", children=[
+            html.P(id="total_PL", children=str(port.total_profit_loss) + " €")
+        ]),
+
+        html.Div(className="three columns", children=[
+            html.P(id="total_PL_rel", children=str(port.total_profit_loss_rel) + " %")
+        ]),
+    ]),
+
+    html.Div(children=[
+        html.H2(children="Asset Allocation",
+                style={"textAlign": "center", "marginTop": 50}),
+        dcc.Graph(
+            id='allocation',
+            figure=allocation,
+            style={"font_size": "2em"}
+        )
+    ]),
+
+    html.Div(children=[
+        html.H2(children="Holdings & Performance",
+                style={"textAlign": "center"}),
+        dash_table.DataTable(
+            id="table",
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict(orient='records'),
+            style_table={'overflowX': 'scroll'},
+            style_data={"font-size": "1.5em", "textAlign": "center"},
+            style_header={"font-size": "1.6em", "font-weight": "bold", "height": "50px", "textAlign": "center"}
+        ),
+    ]),
+
+    html.Div(children=[
+        html.H2(children="Profits & Losses per Position",
+                style={"textAlign": "center"}),
+        dcc.Graph(
+            id='holdings_performance',
+            figure=holdings_perf
+        )
+    ],style={"marginTop": 50})
+
+], style={"border": "50px white solid"})
+
+
+@app.callback(Output("table", "data"),
+              Output("allocation", "figure"),
+              Output("holdings_performance", "figure"),
+              Output("total_value","children"),
+              Output("total_invest","children"),
+              Output("total_PL","children"),
+              Output("total_PL_rel","children"),
+              Input("refresh", "n_clicks"))
+def update(n_clicks):
+    print("Update triggered!")
+    port.load_portfolio()
+    port.update_portfolio()
+    df = port.portfolio
+    figure1 = px.pie(df, values="Current Value", names="Name")
+    figure2 = px.bar(df, x="Name", y= "Profit / Loss")
+    total_worth = str(port.total_worth)+" €"
+    total_invest = str(port.total_investment)+" €"
+    total_pl = str(port.total_profit_loss)+" €"
+    total_pl_rel = str(port.total_profit_loss_rel)+" %"
+
+    return df.to_dict(orient='records'), figure1, figure2, total_worth, total_invest, total_pl, total_pl_rel
+
+
+app.run_server(debug=False, host="0.0.0.0", port=8085)
+
+
+
+
+
